8e88e7b024d103bce1ef8b2d142d318fc9537ee4
Prasad Khanapure <pnkhanapure@gmail.com>
2026-02-08 22:37:21 +0530

    currying updated

--- src/String/currying.js
+++ src/String/currying.js
@@ -1,9 +1,25 @@
-const curringFunction = (x) => {
+// firstSumFunction & secondSumFunction is one and same!!
+const firstSumFunction = (x) => {
   return (y) => {
     return (z) => {
-      console.log(x + y + z);
+      return x + y + z;
     };
   };
 };
+console.log(firstSumFunction(1)(2)(3)); //6
 
-curringFunction(10)(20)(30);
+const secondSumFunction = (x) => (y) => (z) => x + y + z;
+console.log(secondSumFunction(1)(2)(3)); //6
+
+// thirdSumFunction & fourthSumFunction are oen & same!!
+const thirdSumFunction = (a) => (b) => (b ? thirdSumFunction(a + b) : a);
+console.log(thirdSumFunction(1)(2)(3)()); //6
+
+const fourthSumFunction = (a) => {
+  return (b) => {
+    if (b) return fourthSumFunction(a + b);
+    return a;
+  };
+};
+
+console.log(fourthSumFunction(1)(2)(3)()); // 6
--- src/index.js
+++
@@ -1,1 +0,0 @@
-console.log("Testing it via iPad Codesandbox!!");
